6912e046ffce7a4666095b280897cd0ee5ab814a
pica <xi_aochi@163.com>
2023-07-16 14:05:29 +0800

    chatlog

--- html/adminxchatlog.php
+++ html/adminxchatlog.php
@@ -14,17 +14,22 @@ $username = $_SESSION['user']["username"];
 
 if (isset($_GET['action']) && $_GET['action'] === 'gen_user') {
 }
+if (isset($_GET['cid'])) {
+    echo '<pre>' . json_encode(getConversationRecords($_GET['cid'])) . '</pre>';
+    echo "<script>let data = JSON.parse(document.querySelector('pre').textContent);
+    let formattedData = JSON.stringify(data, undefined, 2);
+    document.querySelector('pre').textContent = formattedData;</script>";
+    exit;
+}
 
 $uid = isset($_GET['uid']) ? intval($_GET['uid']) : 0;
 $gt = isset($_GET['gt']) ? intval($_GET['gt']) : 0;
 $limit = 10; // 每页显示的记录数
 
-$sql = "SELECT * FROM chatlog where uid=? and id>? LIMIT $limit";
-$stmt = executePreparedStmt($sql, [$uid, $gt]);
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$rows = getUserConversations($uid);
 
-$sql = "SELECT COUNT(*) as count FROM users ";
-$stmt = executePreparedStmt($sql, []);
+$sql = "SELECT COUNT(*) as count FROM user_conversations where user_id=?  ";
+$stmt = executePreparedStmt($sql, [$uid]);
 $countResult = $stmt->fetch(PDO::FETCH_ASSOC);
 $totalRowCount = $countResult['count'];
 
--- html/db.php
+++ html/db.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'lib.php';
+
 function getInitializedPDO()
 {
     static $pdo;
