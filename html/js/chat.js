5b057fba6dbb4f2470898789475203965842c789
pica <xi_aochi@163.com>
2023-08-23 01:45:34 +0800

    yes

--- html/js/chat.js
+++ html/js/chat.js
@@ -481,8 +481,10 @@ function deleteConversation(index) {
 
 function createConversationElement(id, conversationTitle) {
     var liElement = $('<li class="conversation"></li>').data('id', id);
-    liElement.prepend('<span class="conversation-icon">' + '<svg role="img" width="16px" height="16px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>The Conversation</title><path d="M23.996 10.543c-.131-4.91-4.289-8.773-9.2-8.773H9.005a8.997 8.997 0 0 0-5.957 15.746L1.05 22.23l4.942-2.98c.95.36 1.964.524 3.012.524h6.024c5.04 0 9.099-4.156 8.969-9.23zm-8.937 5.958H9.07c-2.587 0-5.205-2.03-5.696-4.583a5.724 5.724 0 0 1 5.63-6.874h5.99c2.586 0 5.205 2.03 5.696 4.582.688 3.667-2.095 6.875-5.63 6.875z"/></svg>      </span>');
-    liElement.append($('<span class="conversation-list-item-text"></span>').text(conversationTitle));
+    var span=$('<span></span>')
+    span.prepend('<span class="conversation-icon">' + '<svg role="img" width="16px" height="16px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>The Conversation</title><path d="M23.996 10.543c-.131-4.91-4.289-8.773-9.2-8.773H9.005a8.997 8.997 0 0 0-5.957 15.746L1.05 22.23l4.942-2.98c.95.36 1.964.524 3.012.524h6.024c5.04 0 9.099-4.156 8.969-9.23zm-8.937 5.958H9.07c-2.587 0-5.205-2.03-5.696-4.583a5.724 5.724 0 0 1 5.63-6.874h5.99c2.586 0 5.205 2.03 5.696 4.582.688 3.667-2.095 6.875-5.63 6.875z"/></svg>      </span>');
+    span.append($('<span class="conversation-list-item-text"></span>').text(conversationTitle));
+    liElement.prepend(span);
     liElement.append('<button class="delete-button" >&times;</button>');
     return liElement;
 }
