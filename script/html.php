b8e04c6b28cec8ef5f57c1dec65535435eea645b
pica <xi_aochi@163.com>
2023-08-29 13:09:22 +0800

    ok

--- script/html.php
+++ script/html.php
@@ -1,32 +1,36 @@
 <?php
-$directory = __DIR__ . "/../html"; // 指定要处理的目录
-$files = scandir($directory); // 获取目录中的所有文件
+$directories = array(
+    __DIR__ . "/../html",           // Original directory
+    __DIR__ . "/../html/adminxx"    // Additional directory
+);
 
-foreach ($files as $file) {
-    if (pathinfo($file, PATHINFO_EXTENSION) == "html") { // 只处理.html文件
-        $filePath = $directory . "/" . $file;
-        $lines = file($filePath); // 按行读取文件内容
+foreach ($directories as $directory) {
+    $files = scandir($directory);
 
-        $updatedContent = ""; // 存储处理后的文件内容
-        foreach ($lines as $line) {
-            // print_r($line);
-            if (preg_match('/<(script|link).*?(href|src)="([^"]+)"/', $line, $matches)) { // 匹配 <script> 或 <link> 标签的 href 属性值
-                // print_r($line);
-                $matchedHref = $matches[3];
-                $matchedHref = trim($matchedHref);
-                $filename = explode('?', $matchedHref)[0];
-                $filepath = $directory . "/" . $filename;
+    foreach ($files as $file) {
+        if (pathinfo($file, PATHINFO_EXTENSION) == "html") {
+            $filePath = $directory . "/" . $file;
+            $lines = file($filePath);
 
-                if (file_exists($filepath)) { // 检查文件是否存在
-                    $md5 = md5_file($filepath); // 对文件进行 md5 哈希
-                    $updatedLine = str_replace($matchedHref, $filename . "?v=" . $md5, $line); // 在原文件名后面加上 ?v=md5
-                    $line = $updatedLine;
+            $updatedContent = "";
+            foreach ($lines as $line) {
+                if (preg_match('/<(script|link).*?(href|src)="([^"]+)"/', $line, $matches)) {
+                    $matchedHref = $matches[3];
+                    $matchedHref = trim($matchedHref);
+                    $filename = explode('?', $matchedHref)[0];
+                    $filepath = $directory . "/" . $filename;
+
+                    if (file_exists($filepath)) {
+                        $md5 = md5_file($filepath);
+                        $updatedLine = str_replace($matchedHref, $filename . "?v=" . $md5, $line);
+                        $line = $updatedLine;
+                    }
                 }
+
+                $updatedContent .= $line;
             }
 
-            $updatedContent .= $line;
+            file_put_contents($filePath, $updatedContent);
         }
-
-        file_put_contents($filePath, $updatedContent); // 将处理后的内容写入文件
     }
 }
