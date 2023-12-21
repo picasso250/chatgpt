d77b36035c0bc1d34113fbdd7e20801982a484cf
pica <xi_aochi@163.com>
2023-12-21 15:48:02 +0800

    每天消耗的积分

--- html/adminxx/statistics.php
+++ html/adminxx/statistics.php
@@ -27,17 +27,43 @@ $sql = "SELECT DATE(last_updated) AS date, COUNT(*) AS count, SUM(click_recharge
         GROUP BY DATE(last_updated)
         ORDER BY DATE(last_updated)";
 $stmt = executePreparedStmt($sql, [':days' => $days]);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$resultUser = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// 获取当前日期
+$currentDate = date('Y-m-d');
+
+// 计算开始日期
+$startDate = date('Y-m-d', strtotime("-$days day", strtotime($currentDate)));
+
+// 查询 statistics 表中指定日期范围内的数据
+$sql = "SELECT * FROM statistics WHERE date BETWEEN :start_date AND :end_date ORDER BY date ASC";
+$params = [
+    ':start_date' => $startDate,
+    ':end_date' => $currentDate,
+];
+$stmt = executePreparedStmt($sql, $params);
+$resultStatistics = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// 遍历查询结果
+foreach ($resultStatistics as $key => &$row) {
+    // 如果不是第一行，计算差值并存入数组
+    if ($key > 0) {
+        $currentValue = $row['total_points']; 
+        $previousValue = $resultStatistics[$key - 1]['total_points']; 
+        
+        // 计算差值并存入数组
+        $dailyDifference = $currentValue - $previousValue;
+        $row['day_points'] = $dailyDifference;
+    }else{
+        $row['day_points'] = 0;
+    }
+}
 
 // 格式化结果为JavaScript数组
-$jsResult = [];
-foreach ($result as $row) {
-    $jsResult[] = [
-        'date' => $row['date'],
-        'count' => $row['count'],
-        'click_recharge_dialog' => $row['click_count']
-    ];
-}
+$jsResult = [
+    'users' => $resultUser,
+    'statistics' => $resultStatistics
+];
 
 if (isset($_GET['days'])) {
     outputJson($jsResult);
